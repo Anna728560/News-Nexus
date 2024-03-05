a2a6d83022d7f22f0151cbdc63716497cad5b176
Polonnikova Anna <xxxuko@gmail.com>
2024-03-05 17:12:22 +0200

    add models and migration

--- newspaper_agency/models.py
+++ newspaper_agency/models.py
@@ -1,3 +1,46 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ("username", "first_name", "last_name",)
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="newspapers"
+    )
+
+    class Meta:
+        ordering = ("-published_date",)
+
+    def __str__(self) -> str:
+        return self.title
