8853b4d03211382fb3413ec96f06f3b9058f49af
Polonnikova Anna <xxxuko@gmail.com>
2024-03-06 02:24:01 +0200

    add newspaper-page-detail with full text

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, get_topic
+from .views import index, get_topic, newspaper_detail
 
 urlpatterns = [
     path("", index, name="index"),
-    path("topic/<int:pk>/", get_topic, name="get-topic-info")
+    path("topic/<int:pk>/", get_topic, name="get-topic-info"),
+    path("newspaper/<int:pk>/", newspaper_detail, name="newspaper-detail"),
 ]
 
 app_name = "newspaper-agency"
