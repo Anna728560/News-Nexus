a98621d42d429f1448fd43a796b434f8c84064e6
Polonnikova Anna <xxxuko@gmail.com>
2024-03-06 11:49:19 +0200

    add page with newspaper creation (empty)

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from .views import index, get_topic, newspaper_detail
+from .views import (
+    index,
+    get_topic,
+    newspaper_detail,
+    crete_newspaper
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("topic/<int:pk>/", get_topic, name="get-topic-info"),
     path("newspaper/<int:pk>/", newspaper_detail, name="newspaper-detail"),
+    path("newspaper/create-newspaper/", crete_newspaper, name="create-newspaper"),
 ]
 
 app_name = "newspaper-agency"
