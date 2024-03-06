476efe7feb8168f54c7fffaaa0de07b53624a77a
Polonnikova Anna <xxxuko@gmail.com>
2024-03-06 11:35:39 +0200

    add static files to project

--- news_nexus/settings.py
+++ news_nexus/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (BASE_DIR / "static",)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / "news_nexus/static",),
+]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
