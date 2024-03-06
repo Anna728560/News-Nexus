854874e3c9aef17bc77318019b05bd8c64e1e11b
Polonnikova Anna <xxxuko@gmail.com>
2024-03-06 17:04:40 +0200

    add django debug toolbar and customazed admin pannel

--- news_nexus/urls.py
+++ news_nexus/urls.py
@@ -18,6 +18,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns = [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ] + urlpatterns
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
