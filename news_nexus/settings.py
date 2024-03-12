e98772adeb003d8cea2d09c9a07e9e594214360f
Polonnikova Anna <xxxuko@gmail.com>
2024-03-12 21:13:43 +0200

    upd allowed hosts

--- news_nexus/settings.py
+++ news_nexus/settings.py
@@ -39,7 +39,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "news-nexus-btd2.onrender.com/",
+    "news-nexus.onrender.com",
 ]
 
 INTERNAL_IPS = [
