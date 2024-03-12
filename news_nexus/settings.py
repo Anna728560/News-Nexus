baa22a4014d6243717784c9347ab040976539e44
Polonnikova Anna <xxxuko@gmail.com>
2024-03-12 18:59:38 +0200

    update  ALLOWED_HOSTS

--- news_nexus/settings.py
+++ news_nexus/settings.py
@@ -39,7 +39,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "https://news-nexus.onrender.com/",
+    "news-nexus.onrender.com",
 ]
 
 INTERNAL_IPS = [
