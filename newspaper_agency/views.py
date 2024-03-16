5c9da8b06a873c358d1c669f300e20053f1c3543
Polonnikova Anna <xxxuko@gmail.com>
2024-03-16 17:10:59 +0200

    fix queries and add ability to remove newspaper if redactor is in editors of this newspaper

--- newspaper_agency/views.py
+++ newspaper_agency/views.py
@@ -31,13 +31,12 @@ class HomePageView(ListView):
         return context
 
     def get_queryset(self):
-        queryset = super().get_queryset()
+        queryset = super().get_queryset().select_related("topic")
         topic_id = self.request.GET.get("topic")
         if topic_id:
-            topic = get_object_or_404(Topic, pk=topic_id)
-            queryset = queryset.filter(topic=topic)
+            queryset = queryset.filter(topic_id=topic_id)
 
-        return queryset.select_related("topic")
+        return queryset
 
 
 class GetNewspapersByTopic(ListView):
@@ -131,7 +130,7 @@ class CreateCommentView(LoginRequiredMixin, View):
     def post(self, request: HttpRequest, pk: int) -> HttpResponse:
         if not request.user.is_authenticated:
             return redirect("newspaper-agency:login")
-        newspaper = get_object_or_404(Newspaper, pk=pk)
+        newspaper = get_object_or_404(Newspaper.objects.prefetch_related("publishers"), pk=pk)
         form = CreateCommentaryForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
@@ -152,3 +151,12 @@ def add_of_remove_editor_to_authors(request, pk):
     else:
         newspaper.publishers.add(request.user)
     return redirect("newspaper-agency:newspaper-detail", pk=newspaper.pk)
+
+
+@login_required
+def add_of_remove_newspaper(request, pk):
+    """Function that allows to remove newspaper if redactor is in editors."""
+    newspaper = get_object_or_404(Newspaper, pk=pk)
+    # if request.user in newspaper.publishers.all():
+    newspaper.objects.remove(pk=newspaper.pk)
+    return redirect("newspaper-agency:newspaper-home")
